275f918fced7f484a9e9460d288d97d2ee605223
davidjohngee <david.gee@ipengineer.net>
2018-12-07 20:19:26 +0000

    Added onbox

---
+++ OnBoxTraditional/README.md
@@ -0,0 +1,3 @@
+## README
+
+These files are canonical representitives of Op/Event/Commit scripts. You can load them on to Junos and experiment.
---
+++ OnBoxTraditional/overCommit.py
@@ -0,0 +1,7 @@
+import jcs
+
+def main():
+    jcs.syslog("25", "Dodgy Dave has committed too many things in too little time.\n")
+
+if __name__ == '__main__':
+    main()
